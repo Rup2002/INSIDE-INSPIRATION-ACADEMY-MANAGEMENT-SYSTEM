5f356b71c8c9c2e266821e1a3bde8b42b8deff81
Rupsha Debnath <65131961+Rup2002@users.noreply.github.com>
2023-01-28 22:17:09 +0530

    Add files via upload

---
+++ MainMethod/Start.java
@@ -0,0 +1,11 @@
+import FrontEnd.Login;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Login tiasa = new Login();
+		tiasa.main(null);
+	}
+
+}
